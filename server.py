7d39f26bf97c90762843a66306099539bef3e101
Marcin Zepp <nircek-2103@protonmail.com>
2023-11-11 23:07:32 +0100

    Add PySimpleGUI frontend to Serial Port server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import PySimpleGUI as sg
 import bluetooth
 from contextlib import closing
+from datetime import datetime
 
 '''
 
@@ -30,16 +32,50 @@ bluetooth.advertise_service(server_sock, name, **options)
 print(f"Waiting for connection on RFCOMM channel {port}")
 
 client_sock, (client_addr, client_port) = server_sock.accept()
+
 with closing(client_sock) as client_sock:
-    print(f"Accepted connection from {client_addr}/{client_port}")
     server_sock.close()
-    try:
-        while True:
-            data = client_sock.recv(990)
-            if not data:
+
+    sg.theme('Light Brown 3')
+    def receiving_thread(window):
+        try:
+            while True:
+                data = client_sock.recv(990)
+                if not data:
+                    break
+                msg = data.decode().replace("\r\n", "\n")
+                msg = f'{datetime.now().isoformat()} {msg}'
+                window.write_event_value(('-THREAD-', msg), None)
+        except OSError:
+            pass
+
+
+    layout = [
+        [sg.Image(), sg.VSeperator(), sg.Column([
+            [sg.Multiline(size=(127, 30), disabled=True, autoscroll=True, reroute_stdout=True)],
+            [
+                sg.Input(size=(127-50-3, 1), key="-input-"),
+                sg.Submit(size=(50, 1), bind_return_key=True)
+            ]
+        ])]
+    ]
+    sg.theme('Light Purple')
+    window = sg.Window(name, layout, finalize=True)
+
+    print(f"Accepted connection from {client_addr}/{client_port}\n")
+    window.start_thread(lambda: receiving_thread(window), ('-THREAD-', None))
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+        elif event == 'Submit':
+            msg = window['-input-'].get() + '\n'
+            client_sock.send(msg.encode())
+            print(f'{datetime.now().isoformat()} {msg}')
+            window['-input-'].update('')
+        elif event[0] == '-THREAD-':
+            if event[1] is None:
                 break
-            print(data)
-    except OSError:
-        pass
+            print(event[1])
 
 print("Disconnected.")
